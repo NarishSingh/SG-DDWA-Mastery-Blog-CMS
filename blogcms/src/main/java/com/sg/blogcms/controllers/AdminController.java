2f0cefba83ac7b7799a87bfea59ecc0e14549904
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-10-20 22:39:10 -0400

    Admin controller work

--- blogcms/src/main/java/com/sg/blogcms/controllers/AdminController.java
+++ blogcms/src/main/java/com/sg/blogcms/controllers/AdminController.java
@@ -1,7 +1,15 @@
 package com.sg.blogcms.controllers;
 
+import com.sg.blogcms.entity.Role;
+import com.sg.blogcms.entity.User;
 import com.sg.blogcms.model.RoleDao;
 import com.sg.blogcms.model.UserDao;
+import java.util.HashSet;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -11,25 +19,65 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
-    
+
     @Autowired
     RoleDao rDao;
     @Autowired
     UserDao uDao;
     @Autowired
     PasswordEncoder encoder;
-    
+    Set<ConstraintViolation<User>> violations = new HashSet<>();
+
     /**
      * GET - load subdomain with all users
+     *
      * @param model {Model} holds all users, regardless of role
      * @return {String} load subdomain
      */
     @GetMapping("/admin")
-    public String displayAdminPage(Model model){
+    public String displayAdminPage(Model model) {
         model.addAttribute("roles", rDao.readAllRoles());
         model.addAttribute("users", uDao.readAllUsers());
-        
+        model.addAttribute("errors", violations);
+
         return "admin";
     }
-    
+
+    /**
+     * POST create a new user account from form data
+     *
+     * @param request {HttpServletRequest} marshals form data for obj
+     *                construction and validation
+     * @return {String} reload admin subdomain
+     */
+    @PostMapping("/addUser")
+    public String addUser(HttpServletRequest request) {
+        User user = new User();
+        String usernameString = request.getParameter("username");
+        String passwordString = request.getParameter("password");
+
+        if (usernameString.isBlank() || passwordString.isBlank()) {
+            return "admin";
+        } else {
+            user.setUsername(usernameString);
+            user.setPassword(encoder.encode(passwordString));
+            user.setEnabled(true); //admin creates accounts, assume true
+            user.setFirstName(request.getParameter("firstName"));
+            user.setLastName(request.getParameter("lastName"));
+            user.setEmail(request.getParameter("email"));
+
+            Set<Role> userRoles = new HashSet<>();
+            userRoles.add(rDao.readRoleById(Integer.parseInt(request.getParameter("roleId"))));
+            user.setRoles(userRoles);
+        }
+
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(user);
+
+        if (violations.isEmpty()) {
+            uDao.createUser(user);
+        }
+
+        return "redirect:/admin";
+    }
 }
